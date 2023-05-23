ced00ca79dcef1f0c1c316e46a848c60947beef9
Admin <Admin@HP_GAMING>
2023-05-23 22:04:58 +0300

    Add project files.

---
+++ PSDomOOP/Team.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSDomOOP
+{
+    public class Team
+    {
+        public Coach Coach { get; set; }
+        public List<FootballPlayer> Players { get; set; }
+
+        public double AverageAge
+        {
+            get { return Players.Average(p => p.Age); }
+        }
+    }
+}
